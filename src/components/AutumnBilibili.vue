75a2b8f82bc83b6dd9fd1ac2ceb71f6c236dc321
Peko <peko.lai@minpayment.com>
2021-02-08 16:49:58 +0800

    [update] 添加动态效果

--- src/components/AutumnBilibili.vue
+++ src/components/AutumnBilibili.vue
@@ -1,21 +1,67 @@
 <template>
   <div>
-<!--    <a-col :xm="22" :sm="22" :offset="1">-->
-<!--      <header class="autumnHeaderClass">-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a1.png" class="autumnImgClass"></div>-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a2_1.png" class="autumnImgClass" id="girlPic"></div>-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a3.png" class="autumnImgClass"></div>-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a4.png" class="autumnImgClass"></div>-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a5.png" class="autumnImgClass"></div>-->
-<!--        <div class="autumnDivClass"><img src="../assets/autumn/a6.png" class="autumnImgClass"></div>-->
-<!--      </header>-->
-<!--    </a-col>-->
+    <a-col :xm="22" :sm="22" :offset="1">
+      <header class="autumnHeaderClass" @mousemove="mouseUpdate">
+        <div class="autumnDivClass"><img src="../assets/autumn/a1.png" class="autumnImgClass"></div>
+        <div class="autumnDivClass"><img src="../assets/autumn/a2_1.png" class="autumnImgClass" ref="girlPic">></div>
+        <div class="autumnDivClass"><img src="../assets/autumn/a3.png" class="autumnImgClass">></div>
+        <div class="autumnDivClass"><img src="../assets/autumn/a4.png" class="autumnImgClass">></div>
+        <div class="autumnDivClass"><img src="../assets/autumn/a5.png" class="autumnImgClass">></div>
+        <div class="autumnDivClass"><img src="../assets/autumn/a6.png" class="autumnImgClass">></div>
+      </header>
+    </a-col>
   </div>
 </template>
 
 <script>
     export default {
-        name: "AutumnBilibili"
+      name: "AutumnBilibili",
+      mounted() {
+        this.girlAnimation();
+      },
+      data(){
+        return{
+          x:0,
+        }
+      },
+      methods: {
+        mouseUpdate() {
+          let images = document.querySelectorAll('.autumnImgClass');
+          document.querySelector('.autumnHeaderClass').addEventListener('mousemove', (e) => {
+            let percentage = e.clientX / window.outerWidth;
+            let offset = 10 * percentage;
+            let blur = 20;
+
+            for (let [index, image] of images.entries()) {
+              offset *= 1.3;
+
+              let blurValue = (Math.pow((index / images.length - percentage), 2) * blur);
+
+              image.style.setProperty('--offset', `${offset}px`);
+              image.style.setProperty('--blur', `${blurValue}px`);
+            }
+          });
+        },
+        girlAnimation() {
+          setInterval(this.eyes, 4000);
+        },
+        eyes() {
+          let inputDOM = this.$refs.girlPic;
+          setTimeout(function () {
+            inputDOM.setAttribute("src", "../assets/autumn/a2_2.png");
+          }, 500);
+          setTimeout(function () {
+            inputDOM.setAttribute("src", "../assets/autumn/a2_3.png");
+          }, 600);
+          setTimeout(function () {
+            inputDOM.setAttribute("src", "../assets/autumn/a2_2.png");
+          }, 700);
+          setTimeout(function () {
+            inputDOM.setAttribute("src", "../assets/autumn/a2_1.png");
+          }, 800);
+
+        },
+      }
     }
 </script>
 
